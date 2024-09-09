b30f36d2cf7f832cf8a2b62c843be5fc83840211
Alisson <alissonpintor@gmail.com>
2024-09-09 16:36:59 -0400

    alterado nicegui

--- runui.py
+++ runui.py
@@ -1,38 +1,33 @@
 #!/usr/bin/env python3
-import asyncio
-from typing import Optional
+import pandas as pd
+from pandas.api.types import is_bool_dtype, is_numeric_dtype
 
-import httpx
+from nicegui import ui
 
-from nicegui import events, ui
+df = pd.DataFrame(data={
+    'col1': [x for x in range(4)],
+    'col2': ['This', 'column', 'contains', 'strings.'],
+    'col3': [x / 4 for x in range(4)],
+    'col4': [True, False, True, False],
+})
 
-api = httpx.AsyncClient()
-running_query: Optional[asyncio.Task] = None
+
+def update(*, df: pd.DataFrame, r: int, c: int, value):
+    df.iat[r, c] = value
+    ui.notify(f'Set ({r}, {c}) to {value}')
 
 
-async def search(e: events.ValueChangeEventArguments) -> None:
-    """Search for cocktails as you type."""
-    global running_query  # pylint: disable=global-statement # noqa: PLW0603
-    if running_query:
-        running_query.cancel()  # cancel the previous query; happens when you type fast
-    search_field.classes('mt-2', remove='mt-24')  # move the search field up
-    results.clear()
-    # store the http coroutine in a task so we can cancel it later if needed
-    running_query = asyncio.create_task(api.get(f'https://www.thecocktaildb.com/api/json/v1/1/search.php?s={e.value}'))
-    response = await running_query
-    if response.text == '':
-        return
-    with results:  # enter the context of the the results row
-        for drink in response.json()['drinks'] or []:  # iterate over the response data of the api
-            with ui.image(drink['strDrinkThumb']).classes('w-64'):
-                ui.label(drink['strDrink']).classes('absolute-bottom text-subtitle2 text-center')
-    running_query = None
-
-# create a search field which is initially focused and leaves space at the top
-search_field = ui.input(on_change=search) \
-    .props('autofocus outlined rounded item-aligned input-class="ml-3"') \
-    .classes('w-96 self-center mt-24 transition-all')
-results = ui.row()
+with ui.grid(rows=len(df.index)+1).classes('grid-flow-col'):
+    for c, col in enumerate(df.columns):
+        ui.label(col).classes('font-bold')
+        for r, row in enumerate(df.loc[:, col]):
+            if is_bool_dtype(df[col].dtype):
+                cls = ui.checkbox
+            elif is_numeric_dtype(df[col].dtype):
+                cls = ui.number
+            else:
+                cls = ui.input
+            cls(value=row, on_change=lambda event, r=r, c=c: update(df=df, r=r, c=c, value=event.value))
 
 if __name__ in {"__main__", "__mp_main__"}:
     ui.run()
